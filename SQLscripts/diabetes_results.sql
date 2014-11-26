168c94be2b0afaf306b53530f8913009973137db
amywernerallen <amy.werner-allen@arcadiasolutions.com>
2014-11-26 08:37:18 -0500

    adding updated files

--- SQLscripts/diabetes_results.sql
+++ SQLscripts/diabetes_results.sql
@@ -40,4 +40,96 @@ select * from lookup.hcc_icd9
 where CMS_HCC_v12 in (15, 16, 17, 18, 19)
 order by CMS_HCC_v12 
 
-select * from t_vitals
+
+select person_id, pat_site_id
+from mpi.person_patient pp
+join t_patient tp
+on tp.pat_id = pp.pat_id
+
+select medication_name, pp.person_id 
+from t_medicationlist  med
+join t_patient tp
+on tp.pat_id = med.patient_id
+join mpi.person_patient pp
+on tp.pat_id = pp.pat_id
+where medication_name like '%aspirin%'
+and pp.person_id in (
+'156',
+'223',
+'226',
+'230',
+'251',
+'254',
+'272',
+'289',
+'290',
+'323',
+'335',
+'483',
+'543',
+'599',
+'634',
+'637',
+'658',
+'681',
+'708',
+'746',
+'753',
+'782',
+'806',
+'811',
+'815',
+'880',
+'883',
+'937',
+'949',
+'1008',
+'1038',
+'1100',
+'1101',
+'1124',
+'1380',
+'1404',
+'1406',
+'1411',
+'1446',
+'1483',
+'1535',
+'1573',
+'1627',
+'1660',
+'1673',
+'1705',
+'1780',
+'1828',
+'1851',
+'1852',
+'1855',
+'1876',
+'1926',
+'2013',
+'2088',
+'2123',
+'2125',
+'2129',
+'2145',
+'2233',
+'2242',
+'2262',
+'2276',
+'2308',
+'2325',
+'2337',
+'2360',
+'2441',
+'2446',
+'2456',
+'2461',
+'2474',
+'2528',
+'2534',
+'2555',
+'2558',
+'2609' )
+
+
